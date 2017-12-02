ee8d1fcd5b6666ca2761abb084fbadf4dae297cf
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2017-12-02 15:04:42 -0500

    representing instancces

--- buildingNN.py
+++ buildingNN.py
@@ -1,20 +1,31 @@
 import datetime
 import config
 import pymysql.cursors # install with `pip install PyMySQL`
+import random
 
 
 def network(): # do neural net stuff
+	pass
 
 def main():
 	test_set = []
 	training_set = []
 	db = pymysql.connect(host="67.205.179.187", port=3306, user=config.username, password=config.password, db="csci374")
 	cur = db.cursor()
-	cur.execute("SELECT value, recorded FROM meter_data WHERE meter_id = 313 AND DAYOFWEEK(FROM_UNIXTIME(recorded)) IN (1, 2, 3) ORDER BY recorded DESC")
-	for row in cur.fetchall():
-		print("%f recorded on" % float(row[0]), end=' ')
-		print(datetime.datetime.fromtimestamp(int(row[1])).strftime('%Y-%m-%d'))
+	cur.execute("SELECT id, name, area, occupancy, floors, type FROM buildings ORDER BY id ASC LIMIT 3")
+	for building in cur.fetchall():
+		building_id = int(building[0])
+		# print(building_id)
+		cur.execute("SELECT id, name, resource, units FROM meters WHERE building_id = %s", building_id)
+		for meter in cur.fetchall():
+			cur.execute("SELECT value, recorded FROM meter_data WHERE meter_id = %s ORDER BY recorded DESC", int(meter[0]))
+			for data_point in cur.fetchall():
+				if random.randint(0, 100) > 50:
+					test_set.append([(building[1] + ' ' + meter[1]), data_point[0], datetime.datetime.fromtimestamp(int(data_point[1])), meter[3], building[2], building[3], building[4], building[5]])
+				else:
+					training_set.append([(building[1] + ' ' + meter[1]), data_point[0], datetime.datetime.fromtimestamp(int(data_point[1])), meter[3], building[2], building[3], building[4], building[5]])
 	db.close()
-	network(test_set, training_set)
+	print(training_set[0])
+	# network(test_set, training_set)
 
 main()
