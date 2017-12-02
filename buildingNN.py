66ad31f1fa4705d1b0e93f8f8627f75287cf1fc0
Muci Yu <muciyu@ip-97-83.wireless.oberlin.edu>
2017-12-02 15:36:59 -0500

    added one hot time

--- buildingNN.py
+++ buildingNN.py
@@ -4,6 +4,29 @@ import pymysql.cursors # install with `pip install PyMySQL`
 import tensorflow as tf
 import random
 
+def create_time_list():
+	timelist = []
+	for month in range(1,13):
+		for day in range(1, 32):
+			if month < 10:
+				if day < 10:
+					time = '0'+str(month)+'-'+'0'+str(day)
+				else:
+					time = '0' + str(month) + '-' + str(day)
+			else:
+				if day < 10:
+					time = str(month) + '-' + '0' + str(day)
+				else:
+					time = str(month) + '-' + str(day)
+			timelist.append(time)
+	return timelist
+
+def onehot(value, list):
+	onehot_value = [0] * len(list)
+	i = list.index(value)
+	onehot_value[i] = 1
+	return onehot_value
+
 
 def network(): # do neural net stuff
 	pass
@@ -11,6 +34,7 @@ def network(): # do neural net stuff
 def main():
 	test_set = []
 	training_set = []
+	timelist = create_time_list()
 	db = pymysql.connect(host="67.205.179.187", port=3306, user=config.username, password=config.password, db="csci374")
 	cur = db.cursor()
 	cur.execute("SELECT id, name, area, occupancy, floors, type FROM buildings ORDER BY id ASC LIMIT 3")
@@ -22,9 +46,9 @@ def main():
 			cur.execute("SELECT value, recorded FROM meter_data WHERE meter_id = %s ORDER BY recorded DESC", int(meter[0]))
 			for data_point in cur.fetchall():
 				if random.randint(0, 100) > 50:
-					test_set.append([(building[1] + ' ' + meter[1]), data_point[0], datetime.datetime.fromtimestamp(int(data_point[1])), meter[3], building[2], building[3], building[4], building[5]])
+					test_set.append([(building[1] + ' ' + meter[1]), data_point[0], onehot(datetime.datetime.fromtimestamp(int(data_point[1])).strftime('%m-%d'), timelist), building[2], building[3], building[4], building[5]])
 				else:
-					training_set.append([(building[1] + ' ' + meter[1]), data_point[0], datetime.datetime.fromtimestamp(int(data_point[1])), meter[3], building[2], building[3], building[4], building[5]])
+					training_set.append([(building[1] + ' ' + meter[1]), data_point[0], onehot(datetime.datetime.fromtimestamp(int(data_point[1])).strftime('%m-%d'), timelist), building[2], building[3], building[4], building[5]])
 	db.close()
 	print(training_set[0])
 	# network(test_set, training_set)
