a169a2eecf64c01a9457557f0ff2bece366bdea2
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2017-12-02 14:37:12 -0500

    asdfkb

--- buildingNN.py
+++ buildingNN.py
@@ -2,10 +2,19 @@ import datetime
 import config
 import pymysql.cursors # install with `pip install PyMySQL`
 
-db = pymysql.connect(host="67.205.179.187", port=3306, user=config.username, password=config.password, db="csci374")
-cur = db.cursor()
-cur.execute("SELECT value, recorded FROM meter_data WHERE meter_id = 313 AND DAYOFWEEK(FROM_UNIXTIME(recorded)) IN (1, 2, 3) ORDER BY recorded DESC")
-for row in cur.fetchall():
-	print("%f recorded on" % float(row[0]), end=' ')
-	print(datetime.datetime.fromtimestamp(int(row[1])).strftime('%Y-%m-%d'))
-db.close()
+
+def network(): # do neural net stuff
+
+def main():
+	test_set = []
+	training_set = []
+	db = pymysql.connect(host="67.205.179.187", port=3306, user=config.username, password=config.password, db="csci374")
+	cur = db.cursor()
+	cur.execute("SELECT value, recorded FROM meter_data WHERE meter_id = 313 AND DAYOFWEEK(FROM_UNIXTIME(recorded)) IN (1, 2, 3) ORDER BY recorded DESC")
+	for row in cur.fetchall():
+		print("%f recorded on" % float(row[0]), end=' ')
+		print(datetime.datetime.fromtimestamp(int(row[1])).strftime('%Y-%m-%d'))
+	db.close()
+	network(test_set, training_set)
+
+main()
