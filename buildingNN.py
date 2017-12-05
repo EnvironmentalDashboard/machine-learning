eb3b8be95267a3e7922e6cb04810f48bac61e645
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2017-12-05 14:00:51 -0500

    taking diff approach

--- buildingNN.py
+++ buildingNN.py
@@ -2,7 +2,10 @@ import datetime
 import config
 import pymysql.cursors # install with `pip install PyMySQL`
 import tensorflow as tf
-import random
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.layers import LSTM
 
 def create_time_list():
 	timelist = []
@@ -27,83 +30,65 @@ def onehot(value, list):
 	onehot_value[i] = 1
 	return onehot_value
 
-
-def network(): # do neural net stuff
-	pass
+def create_model(layer1 = 1, layer2 = 50, layer3 = 100, layer4 = 1): # do neural net stuff
+	model = Sequential()
+	model.add(LSTM(
+    input_shape=(layer2, layer1),
+    output_dim=layer2,
+    return_sequences=True))
+	model.add(Dropout(0.2))
+	model.add(LSTM(layer3, return_sequences=False))
+  model.add(Dropout(0.2))
+  model.add(Dense(output_dim=layer4))
+  model.add(Activation("linear"))
+  model.compile(loss="mse", optimizer="rmsprop")
+  return model
 
 def main():
+	epochs = 1
+	instances = {}
 	test_set = []
 	training_set = []
 	timelist = create_time_list()
 	db = pymysql.connect(host="67.205.179.187", port=3306, user=config.username, password=config.password, db="csci374")
 	cur = db.cursor()
-	cur.execute("SELECT DISTINCT resource FROM meters")
-	resources = []
-	for resource in cur.fetchall():
-		resources.append(resource[0])
-	cur.execute("SELECT DISTINCT type FROM buildings")
-	building_types = []
-	for btype in cur.fetchall():
-		building_types.append(btype[0])
-	cur.execute("SELECT id, area, occupancy, floors, type FROM buildings ORDER BY id ASC LIMIT 3") # take limit off when not testing
-	for building in cur.fetchall():
-		building_id = int(building[0])
-		cur.execute("SELECT id, uuid, resource FROM meters WHERE building_id = %s", building_id)
-		for meter in cur.fetchall():
-			cur.execute("SELECT value, recorded FROM meter_data WHERE meter_id = %s AND value IS NOT NULL ORDER BY recorded DESC", int(meter[0]))
-			for data_point in cur.fetchall():
-				if random.randint(0, 100) > 50:
-					test_set.append([(meter[1]), data_point[0], onehot(datetime.datetime.fromtimestamp(int(data_point[1])).strftime('%m-%d'), timelist), building[1], building[2], building[3], onehot(building[4], building_types), onehot(meter[2], resources)])
-				else:
-					training_set.append([(meter[1]), data_point[0], onehot(datetime.datetime.fromtimestamp(int(data_point[1])).strftime('%m-%d'), timelist), building[1], building[2], building[3], onehot(building[4], building_types), onehot(meter[2], resources)])
+	cur.execute("SELECT id, resource FROM meters ORDER BY resource ASC") # we're going to build a seperate network for each resource type (e.g. electricity, water, gas, etc.)
+	current_resource = None
+	for meter in cur.fetchall():
+		if meter[1] != current_resource:
+			current_resource = meter[1]
+			instances[current_resource] = []
+		cur.execute("SELECT value, recorded FROM meter_data WHERE meter_id = %s ORDER BY recorded DESC", int(meter[0]))
+		last_point = 0
+		for data_point in cur.fetchall():
+			val = data_point[0]
+			if val == None: # very few data points are null so just fill in the ones that are
+				val = last_point
+			instances[current_resource].append(val)
+			last_point = val
+			# instances.append([(meter[1]), data_point[0], onehot(datetime.datetime.fromtimestamp(int(data_point[1])).strftime('%m-%d'), timelist), building[1], building[2], building[3], onehot(building[4], building_types), onehot(meter[2], resources)])
 	db.close()
+	num_instances = len(instances)
+	training_size = int(num_instances/2)
+	window_size = int(num_instances/20)
+	for i in range(num_instances-window_size):
+		if i < training_size:
+			training_set.append(instances[i:i+window_size])
+		else:
+			test_set.append(instances[i:i+window_size])
+	training_set = np.array(training_set)
+	test_set = np.array(test_set)
+	training_set = np.reshape(training_set, (len(training_set), len(training_set[0]), 1))
+	test_set = np.reshape(test_set, (len(test_set), len(test_set[0]), 1))
 	print(training_set[0])
-	# network(test_set, training_set)
-
-def Build_and_run_Network(training, testing, num_attr, num_labels, learning_rate, NUM_NEURONS, numIteration):
-	x = tf.placeholder(tf.float32, shape=[None, num_attr])  # input matrix
-	# weights and biases of hidden layer
-	W_hidden = tf.Variable(tf.truncated_normal([num_attr, NUM_NEURONS], stddev=0.1))
-	b_hidden = tf.Variable(tf.constant(0.1, shape=[NUM_NEURONS]))
-	net_hidden = tf.matmul(x, W_hidden) + b_hidden
-	out_hidden = tf.sigmoid(net_hidden)
-	# output layer
-	W_output = tf.Variable(tf.truncated_normal([NUM_NEURONS, num_labels], stddev=0.1))
-	b_output = tf.Variable(tf.constant(0.1, shape=[num_labels]))
-
-	net_output = tf.matmul(out_hidden, W_output) + b_output
-	# actual labels
-	y = tf.placeholder(tf.float32, shape=[None, num_labels])
-
-	# train
-	prediction = tf.nn.softmax(net_output)
-	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=training['label']))
-
-	optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
-
-	hm_epoch = numIteration
-	with tf.Session() as sess:
-		sess.run(tf.global_variables_initializer())
-		for epoch in range(1, hm_epoch + 1):
-			epoch_loss = 0
-			_, c = sess.run([optimizer, cost], feed_dict={x: training['attr'], y: training['label']})
-			epoch_loss += c
-			if epoch % 50 == 0:
-				# print('Epoch', epoch, "comleted out of", hm_epoch, '| loss: ', epoch_loss)
-				correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-				accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-				accuracy_float = round(float(sess.run(accuracy, feed_dict={x: training['attr'], y: training['label']})), 5)
-				print('Accuracy:', accuracy_float)
-
-		# evaluate
-		num_test = len(testing['attr'])
-		num_train = len(training['attr'])
-		print('The size of training set: %s\nThe size of test set: %s'%(num_train, num_test))
-
-		correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-		accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-		accuracy_float = round(float(sess.run(accuracy, feed_dict={x: testing['attr'], y: testing['label']})), 5)
-		print('Accuracy:', accuracy_float)
-		# print('95% confidence interval: ', cf(accuracy_float, num_test, 1.96))
+	model = create_model()
+	model.fit(
+    X_train,
+    y_train,
+    batch_size=512,
+    nb_epoch=epochs,
+    validation_split=0.05,
+    shuffle=False)
+	# predictions = lstm.predict_sequences_multiple(model, X_test, seq_len, 50)
 
 main()
