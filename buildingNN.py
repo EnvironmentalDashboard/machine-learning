7c653c6b9090d3bc166a29216e24e2d192b049be
Muci Yu <muciyu@Mucis-MacBook-Pro.local>
2017-12-08 02:40:39 -0500

    Priliminary prediction

--- buildingNN.py
+++ buildingNN.py
@@ -8,6 +8,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Activation, Dense, Dropout
 from keras.layers import LSTM
+import matplotlib.pyplot as plt
 
 
 def create_time_list():
@@ -75,14 +76,23 @@ def predict_sequences_multiple(model, data, window_size, prediction_len):
         curr_frame = data[i * prediction_len]
         predicted = []
         for j in range(prediction_len):
-            prediction = model.predict(curr_frame[np.newaxis, :, :])[0,0]
-            print('predicted:', prediction)
-
-            predicted.append(prediction)
+            predicted.append(model.predict(curr_frame[np.newaxis, :, :])[0, 0])
             curr_frame = curr_frame[1:]
             curr_frame = np.insert(curr_frame, [window_size - 1], predicted[-1], axis=0)
         prediction_seqs.append(predicted)
     return prediction_seqs
+
+
+def plot_results_multiple(predicted_data, true_data, prediction_len):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+	# Pad the list of predictions to shift it in the graph to it's correct start
+    for i, data in enumerate(predicted_data):
+        padding = [None for p in range(i * prediction_len)]
+        plt.plot(padding + data, label='Prediction')
+        # plt.legend()
+    plt.show()
 
 
 def download_data():
@@ -138,7 +148,6 @@ def build_train_and_test_data(data, window_size):
     x_test = np.array(x_test, dtype=float)
     y_test = np.array(y_test, dtype=float)
 
-
     return x_train, y_train, x_test, y_test
 
 
@@ -153,7 +162,8 @@ def main():
 
         model.fit(x_train, y_train, batch_size=512, nb_epoch=epochs, validation_split=0.05, shuffle=False)
         predictions = predict_sequences_multiple(model, x_test, window_size, 7)
-        print("What is this printing?", predictions)
+        print(len(x_test), len(y_test), len(predictions))
+        plot_results_multiple(predictions, y_test, 7)
 
 
 main()
