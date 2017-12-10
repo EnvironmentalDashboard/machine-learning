50970fb2ccc99900f283a4c3ea6bd310af815efa
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2017-12-10 11:40:43 -0500

    going to download hour res data

--- bos/meter_data.php
+++ bos/meter_data.php
@@ -3,8 +3,8 @@ require_once 'class.bos.php';
 ignore_user_abort(1);
 set_time_limit(0);
 $bos = new BuildingOS($db);
-$res = 'day';
-$chunk = 8640000; // download data in 100 day chunks
+$res = 'hour';
+$chunk = 604800; // download data in week chunks
 foreach ($db->query('SELECT id, url FROM meters') as $meter) {
 	$bos->updateMeter($meter['id'], $meter['url'] . '/data', $res, $chunk);
 	sleep(3); // don't bombard bos api
