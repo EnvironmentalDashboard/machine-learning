c55cc3b587e742fe5bfb0396b84c9fe7a28af36b
Muci Yu <muciyu@ip-97-83.wireless.oberlin.edu>
2017-12-15 14:00:01 -0500

    added a lot of testing charts and stuff

--- buildingNN.py
+++ buildingNN.py
@@ -6,6 +6,7 @@ import datetime
 import sys
 import os
 import random
+import csv
 import config
 import pymysql.cursors
 import tensorflow as tf
@@ -51,15 +52,19 @@ def predict_sequences_multiple(model, data, window_size):
     return prediction_seqs
 
 
-def plot_results_multiple(predicted_data, true_data, prediction_len):
+def plot_results_multiple(predicted_data, true_data, prediction_len, name, MSE, NRMSD, res):
     fig = plt.figure(facecolor='white')
+    plt.title('MeterID=%s Epochs=%s WS=%s NN=%s %s'%name)
+    plt.annotate("MSE = %s\nNRMSD = %s"%(round(MSE, 3), round(NRMSD, 3)), xy = (0.75, 0.05), xycoords = 'axes fraction')
+    plt.xlabel("Unit = %s"%res)
+    plt.ylabel("Standardized Usage")
     ax = fig.add_subplot(111)
-    ax.plot(true_data, label='True Data')
-	# Pad the list of predictions to shift it in the graph to it's correct start
+    ax.plot(true_data, label='Actual Data')
+    plt.legend()
+    # Pad the list of predictions to shift it in the graph to it's correct start
     for i, data in enumerate(predicted_data):
         padding = [None for p in range(i * prediction_len)]
         plt.plot(padding + data, label='Prediction')
-        # plt.legend()
     plt.show()
     return fig
 
@@ -129,21 +134,25 @@ def build_train_and_test_data(data, window_size, training_pct, normal_in_window)
     #     y_test = [[x_test[i][-1]] for i in range(1, len(x_test))] + [x_test[-1][-1]]
     # else:
 
-    # training_set = normalize_data(train_array)
-    # print("===========normalized training=========", training_set)
-    # test_set = normalize_data(test_array)
-    # x_train, y_train = make_batches(window_size, training_set)
-    # x_test, y_test = make_batches(window_size, test_set)
+    training_set = normalize_data(train_array)
+    print("===========normalized training=========", training_set)
+    test_set = normalize_data(test_array)
+    x_train, y_train = make_batches(window_size, training_set)
+    x_test, y_test = make_batches(window_size, test_set)
+    normalization = 'StandardScaler'
 
-    for i in range(len(train_array)):
-        train_array[i] = [convertRange(train_array[i], old_min_train, old_max_train, 0, 10)]
-    for i in range(len(test_array)):
-        test_array[i] = [convertRange(test_array[i], old_min_test, old_max_test, 0, 10)]
-    x_train, y_train = make_batches(window_size, train_array)
-    x_test, y_test = make_batches(window_size, test_array)
-    print('x_train =====', x_train)
-    print('y_train =====', y_train)
+    # for i in range(len(train_array)):
+    #     train_array[i] = [convertRange(train_array[i], old_min_train, old_max_train, 0, 1)]
+    # for i in range(len(test_array)):
+    #     test_array[i] = [convertRange(test_array[i], old_min_test, old_max_test, 0, 1)]
+    # x_train, y_train = make_batches(window_size, train_array)
+    # x_test, y_test = make_batches(window_size, test_array)
+    # print('x_train =====', x_train)
+    # print('y_train =====', y_train)
+    # normalization = 'CovertRange'
+
     diff = max(y_test)[0]-min(y_test)[0]
+
 
     x_train = np.array(x_train, dtype=float)
     y_train = np.array(y_train, dtype=float)
@@ -153,7 +162,7 @@ def build_train_and_test_data(data, window_size, training_pct, normal_in_window)
     print('x_train =====', x_train)
     print('y_train =====', y_train)
     print('diff: ',diff)
-    return x_train, y_train, x_test, y_test, diff
+    return x_train, y_train, x_test, y_test, diff, normalization
 
 def windowSize(resolution):
     if resolution == 'day':
@@ -165,8 +174,11 @@ def windowSize(resolution):
 
 def main():
     args = len(sys.argv)
-    epochs = 50
+    epochs = 5
     path = os.getcwd()
+    NN = 20
+    training_pct = 0.9
+    val_pct = 0.1
     if args == 1:
         res = 'hour'
         chart = False
@@ -186,11 +198,11 @@ def main():
         if len(meter[1]) == 0:
             print(meter[0], "has no data")
             continue
-        x_train, y_train, x_test, y_test, diff= build_train_and_test_data(meter, window_size, 0.9, True)
+        x_train, y_train, x_test, y_test, diff, normalization= build_train_and_test_data(meter, window_size, training_pct, True)
 
-        model = create_model(1, window_size, 200, 1)
+        model = create_model(1, window_size, NN, 1)
 
-        model.fit(x_train, y_train, batch_size=32, epochs=epochs, validation_split= 0, shuffle=False)
+        model.fit(x_train, y_train, batch_size=32, epochs=epochs, validation_split= val_pct, shuffle=False)
         MSE = model.evaluate(x_test, y_test)
         print('Accuracy/Mean Squared Error: ', MSE)
         NRMSD = sqrt(MSE)/float(diff)
@@ -198,8 +210,14 @@ def main():
         if chart:
             predictions = predict_sequences_multiple(model, x_test, window_size)
             # print(len(x_test), len(y_test), len(predictions))
-            fig = plot_results_multiple(predictions, y_test, window_size)
-            fig.savefig('id%s_%s_epochs%s_ws%s_nn%s'%(266, res, epochs, window_size, 200))
+            name = 266, epochs, window_size, NN, normalization
+            fig = plot_results_multiple(predictions, y_test, window_size, name, MSE, NRMSD, res)
+            fig.savefig('id%s_%s_epochs%s_ws%s_nn%s_%s'%(266, res, epochs, window_size, NN, normalization))
+
+        with open('recorder.csv', mode='a', newline='') as csvfile:
+            filewriter = csv.writer(csvfile, delimiter = ',')
+            filewriter.writerow([MSE, NRMSD, epochs, window_size, NN, normalization, training_pct, val_pct])
+
         # See https://machinelearningmastery.com/save-load-keras-deep-learning-models/
         model_json = model.to_json()
         model.save_weights(path + "/model.h5") # serialize weights to HDF5 to read from later
